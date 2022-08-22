f2d3d449938ea9c50c9aff6c42c2fd0558463ecd
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-08-22 17:44:09 +0200

    initial commit

--- Program.cs
+++ Program.cs
@@ -2,8 +2,15 @@
 
 class Program
 {
+    public static int AddNumbers(int num1, int num2){
+        int sum = num1 + num2;
+        System.Console.WriteLine(sum);
+        return sum;
+    } 
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
+        AddNumbers(1, 2);
     }
 }
